9535742a5d82a1285d19051e019d24e85a757187
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-09-20 09:37:43 +0900

    Rename utility function

---
+++ tests/test_utils.bash
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Find the root directory of the repository.
+rootdir="$(cd -- "$(dirname -- "$0")/.." && pwd)"
+
+# shellcheck source=../bashu
+source "$rootdir/bashu"
+
+# shellcheck disable=SC2034
+testcase_print_vars() {
+  var1="hello"
+  var2="world"
+  var3="!"
+
+  output=$(print_var_defs "var1" "var2" "var3")
+  [ "$output" == "declare -- var1=\"hello\"; declare -- var2=\"world\"; declare -- var3=\"!\"; " ]
+}
+
+# shellcheck disable=SC2034
+testcase_print_vars_array() {
+  array=("hello" "world" "!")
+
+  output=$(print_var_defs "array")
+  [ "$output" == "declare -a array=([0]=\"hello\" [1]=\"world\" [2]=\"!\"); " ]
+}
+
+# shellcheck disable=SC2034
+testcase_print_vars_associative_array() {
+  declare -A dict
+  dict[e01]="hello"
+  dict[e02]="world"
+  dict[e03]="!"
+
+  output=$(print_var_defs "dict")
+  [ "$output" == "declare -A dict=([e01]=\"hello\" [e03]=\"!\" [e02]=\"world\" ); " ]
+}
+
+# shellcheck disable=SC2034
+testcase_print_integars() {
+  declare -i int1
+  declare -i int2
+  declare -i int3
+  int1=0
+  int2=1
+  int3=2
+
+  output=$(print_var_defs "int1" "int2" "int3")
+  [ "$output" == "declare -i int1=\"0\"; declare -i int2=\"1\"; declare -i int3=\"2\"; " ]
+}
+
+
+
+bashu_main "$@"
