b597b95926583434c0a3b00d7aa42011ff02c6fa
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-09-20 12:22:26 +0900

    Fix initialize function and add test for it

--- tests/test_bashu_main.bash
+++ tests/test_bashu_main.bash
@@ -30,8 +30,31 @@ testcase_specfile() {
   [ "$bashu_specfile" == "$expected" ]
 }
 
-testcase_collect_testcases() {
-  :
+### Initialize
+
+# Set random values to mock variables to be uninitialized.
+_testcase_initialize_setup() {
+  bashu_is_running=$RANDOM
+  bashu_all_testcases=("testcase_dummy")
+  bashu_performed_testcases=("testcase_dummy")
+  bashu_passed_testcases=("testcase_dummy")
+  bashu_failed_testcases=("testcase_dummy")
+}
+
+testcase_initialize() {
+  # Given that randome values are substituted,
+  _testcase_initialize_setup
+  # When `bashu_initialize` is called,
+  bashu_initialize
+  # Then variables are initilized.
+  [ $bashu_is_running -eq 0 ]
+  [ ${#bashu_all_testcases[@]} -eq 0 ]
+  [ ${#bashu_performed_testcases[@]} -eq 0 ]
+  [ ${#bashu_passed_testcases[@]} -eq 0 ]
+  [ ${#bashu_failed_testcases[@]} -eq 0 ]
+
+  # File descriptor for test results is also opened.
+  : >&$bashu_fd_result
 }
 
 bashu_main "$@"
