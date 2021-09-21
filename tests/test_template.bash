59cf48faf472d2edb3dee4e8c92dae63b8c1e2be
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-09-22 01:13:57 +0900

    Add a template spec file

---
+++ tests/test_template.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Find the root directory of the repository.
+rootdir="$(cd -- "$(dirname -- "$0")/.." && pwd)"
+
+# shellcheck source=../bashu
+source "$rootdir/bashu"
+
+testcase_test() {
+  :
+}
+
+bashu_main "$@"
